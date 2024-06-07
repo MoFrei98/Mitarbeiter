05204fe303ab148073f22e93772a275f371edab1
MoFrei98 <morris.freihoff@web.de>
2024-06-07 21:37:07 +0200

    upload

---
+++ Angestellter.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Mitarbeiter.cpp"
+using namespace std;
+
+class Angestellter: public Mitarbeiter {
+private:
+    double gehalt;
+
+public:
+    // Call the constructor of baseclass Mitarbeiter (Mitarbeiter(name, rvNr))
+    Angestellter(string name, int rvNr, double gehalt): Mitarbeiter(name, rvNr), gehalt(gehalt) {
+        // this->gehalt = gehalt;
+    }
+
+    double getGehalt() {
+        return gehalt;
+    }
+
+    void setGehalt(double gehalt) {
+        this->gehalt = gehalt;
+    }
+
+    void mitarbeiterInfo() {
+        Mitarbeiter::mitarbeiterInfo(); // call of the base class method
+        cout << "Status: Angestellter" << endl;
+        cout << "Gehalt: " << gehalt << endl;
+        cout << endl;
+    }
+
+    void tarifErhoehung(double prozent) {
+        gehalt += gehalt * prozent / 100;
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Angestellter.cpp"
+using namespace std;
+
+int main() {
+    // declare the objects
+    Angestellter a1("Max Mustermann", 123456, 3000);    // direct constructor call
+    Angestellter a2 = Angestellter("Maria Musterfrau", 654321, 4000);   // nice constructor call :D
+
+    // test a1
+    cout << "a1 before changes: " << endl;
+    a1.mitarbeiterInfo();
+    a1.setName("Max Mustermann Jr.");
+    a1.tarifErhoehung(10.0);
+    cout << "a1 after changes: " << endl;
+    a1.mitarbeiterInfo();
+
+    cout << "--------------------------------" << endl << endl;
+
+    // test a2
+    cout << "a2 before changes: " << endl;
+    a2.mitarbeiterInfo();
+    a2.setGehalt(4500);
+    a2.setRvNr(34759);
+    cout << "a2 after changes: " << endl;
+    a2.mitarbeiterInfo();
+
+    return 0;
+}
